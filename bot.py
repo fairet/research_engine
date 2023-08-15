8fea4d55ea2a7cec61ad88943c093e5f09191bd8
fairet <anyone@fairet.0day>
2023-08-15 18:46:27 +0500

    Added config.json

--- bot.py
+++ bot.py
@@ -1,21 +1,25 @@
+__author__ = 'fairet'
+
 from aiogram import Bot, Dispatcher, executor, types
-from config import *
+from json import load
 from main import *
 
-bot = Bot(API_TOKEN)
+def parse(one, two):
+    with open ('config', 'r') as read:
+        parsed = load(read)
+
+    return parsed[one][two]
+
+bot = Bot(parse('config', 'API_TOKEN'))
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def on_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, text="Для доступа подайте завяку внизу.")
+    await bot.send_message(message.from_user.id, text=f"(Автор: {__author__}\nДля доступа подайте заявку внизу.")
 
 @dp.message_handler(content_types=['text'])
 async def on_text_message(message: types.Message):
     await bot.send_message(message.from_user.id, text=search(message.text))
 
-
-def main():
+async def main():
     executor.start_polling(dp)
-
-if __name__ == "__main__":
-    main()
